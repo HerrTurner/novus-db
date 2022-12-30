f5ec929545261f7b4e28aead4d4c62144c1dc994
Herr Turner <georgeeturnere@gmail.com>
2022-12-29 19:43:14 -0600

    Added user table

--- novusDB.sql
+++ novusDB.sql
@@ -1,2 +1,18 @@
-CREATE DATABASE novus;
+-- CREATE DATABASE novus;
 USE novus;
+
+CREATE TABLE user (
+id INT NOT NULL AUTO_INCREMENT,
+username VARCHAR(56),
+password VARCHAR(56),
+email VARCHAR(56),
+
+CONSTRAINT username_u
+UNIQUE (username),
+CONSTRAINT email_u
+UNIQUE (email),
+
+PRIMARY KEY(id)
+);
+
+SELECT * FROM user;
